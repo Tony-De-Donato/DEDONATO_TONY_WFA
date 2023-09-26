2cae41f29d3ac25ffe8b1b56bc450f1674929af1
Tony-De-Donato <tonydedo13@gmail.com>
2023-09-26 14:13:48 +0200

    sorry, forgot to commit, here is fonctionnal pong

--- wfa_pong/wfa_pong/Form1.Designer.cs
+++ wfa_pong/wfa_pong/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace wfa_pong
 {
-    partial class Form1
+    partial class Tong
     {
         /// <summary>
         /// Variable nécessaire au concepteur.
@@ -28,35 +28,230 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tong));
+            this.leftPlayer = new System.Windows.Forms.PictureBox();
+            this.rightPlayer = new System.Windows.Forms.PictureBox();
+            this.theBall = new System.Windows.Forms.PictureBox();
+            this.scoreLeft = new System.Windows.Forms.Label();
+            this.scoreRight = new System.Windows.Forms.Label();
+            this.tongLogo = new System.Windows.Forms.PictureBox();
+            this.theTimer = new System.Windows.Forms.Timer(this.components);
+            this.pauseInfo = new System.Windows.Forms.Label();
+            this.pauseMenuCtn = new System.Windows.Forms.FlowLayoutPanel();
+            this.restartBtn = new System.Windows.Forms.Button();
+            this.optionsBtn = new System.Windows.Forms.Button();
+            this.leaveBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.theBall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tongLogo)).BeginInit();
+            this.pauseMenuCtn.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button2
+            // leftPlayer
             // 
-            this.button2.Location = new System.Drawing.Point(457, 111);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 0;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.leftPlayer.BackColor = System.Drawing.Color.Transparent;
+            this.leftPlayer.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("leftPlayer.BackgroundImage")));
+            this.leftPlayer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.leftPlayer.Location = new System.Drawing.Point(-2, 180);
+            this.leftPlayer.Name = "leftPlayer";
+            this.leftPlayer.Size = new System.Drawing.Size(32, 140);
+            this.leftPlayer.TabIndex = 0;
+            this.leftPlayer.TabStop = false;
+            // 
+            // rightPlayer
+            // 
+            this.rightPlayer.BackColor = System.Drawing.Color.Transparent;
+            this.rightPlayer.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rightPlayer.BackgroundImage")));
+            this.rightPlayer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rightPlayer.Location = new System.Drawing.Point(994, 180);
+            this.rightPlayer.Name = "rightPlayer";
+            this.rightPlayer.Size = new System.Drawing.Size(32, 140);
+            this.rightPlayer.TabIndex = 1;
+            this.rightPlayer.TabStop = false;
+            // 
+            // theBall
+            // 
+            this.theBall.BackColor = System.Drawing.Color.Transparent;
+            this.theBall.Image = ((System.Drawing.Image)(resources.GetObject("theBall.Image")));
+            this.theBall.Location = new System.Drawing.Point(492, 234);
+            this.theBall.Name = "theBall";
+            this.theBall.Size = new System.Drawing.Size(41, 41);
+            this.theBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.theBall.TabIndex = 2;
+            this.theBall.TabStop = false;
+            // 
+            // scoreLeft
+            // 
+            this.scoreLeft.AutoSize = true;
+            this.scoreLeft.BackColor = System.Drawing.Color.Transparent;
+            this.scoreLeft.Font = new System.Drawing.Font("Castellar", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLeft.ForeColor = System.Drawing.Color.Aqua;
+            this.scoreLeft.Location = new System.Drawing.Point(370, 55);
+            this.scoreLeft.Name = "scoreLeft";
+            this.scoreLeft.Size = new System.Drawing.Size(36, 34);
+            this.scoreLeft.TabIndex = 3;
+            this.scoreLeft.Text = "0";
+            this.scoreLeft.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // scoreRight
+            // 
+            this.scoreRight.AutoSize = true;
+            this.scoreRight.BackColor = System.Drawing.Color.Transparent;
+            this.scoreRight.Font = new System.Drawing.Font("Castellar", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreRight.ForeColor = System.Drawing.Color.Aqua;
+            this.scoreRight.Location = new System.Drawing.Point(630, 55);
+            this.scoreRight.Name = "scoreRight";
+            this.scoreRight.Size = new System.Drawing.Size(36, 34);
+            this.scoreRight.TabIndex = 4;
+            this.scoreRight.Text = "0";
+            this.scoreRight.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tongLogo
+            // 
+            this.tongLogo.BackColor = System.Drawing.Color.Transparent;
+            this.tongLogo.Location = new System.Drawing.Point(426, 15);
+            this.tongLogo.Name = "tongLogo";
+            this.tongLogo.Size = new System.Drawing.Size(182, 103);
+            this.tongLogo.TabIndex = 5;
+            this.tongLogo.TabStop = false;
+            // 
+            // theTimer
+            // 
+            this.theTimer.Enabled = true;
+            this.theTimer.Interval = 40;
+            this.theTimer.Tick += new System.EventHandler(this.theTimer_Tick);
+            // 
+            // pauseInfo
+            // 
+            this.pauseInfo.AutoSize = true;
+            this.pauseInfo.BackColor = System.Drawing.Color.Transparent;
+            this.pauseInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pauseInfo.ForeColor = System.Drawing.Color.Teal;
+            this.pauseInfo.Location = new System.Drawing.Point(817, 15);
+            this.pauseInfo.Name = "pauseInfo";
+            this.pauseInfo.Size = new System.Drawing.Size(209, 25);
+            this.pauseInfo.TabIndex = 6;
+            this.pauseInfo.Text = "Click SPACE to pause";
+            this.pauseInfo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pauseMenuCtn
+            // 
+            this.pauseMenuCtn.AutoSize = true;
+            this.pauseMenuCtn.BackColor = System.Drawing.Color.Transparent;
+            this.pauseMenuCtn.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pauseMenuCtn.Controls.Add(this.restartBtn);
+            this.pauseMenuCtn.Controls.Add(this.optionsBtn);
+            this.pauseMenuCtn.Controls.Add(this.leaveBtn);
+            this.pauseMenuCtn.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pauseMenuCtn.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.pauseMenuCtn.Location = new System.Drawing.Point(458, 180);
+            this.pauseMenuCtn.Name = "pauseMenuCtn";
+            this.pauseMenuCtn.Size = new System.Drawing.Size(112, 143);
+            this.pauseMenuCtn.TabIndex = 10;
+            this.pauseMenuCtn.TabStop = true;
+            this.pauseMenuCtn.Visible = false;
+            // 
+            // restartBtn
+            // 
+            this.restartBtn.Location = new System.Drawing.Point(3, 3);
+            this.restartBtn.Name = "restartBtn";
+            this.restartBtn.Size = new System.Drawing.Size(102, 37);
+            this.restartBtn.TabIndex = 0;
+            this.restartBtn.TabStop = false;
+            this.restartBtn.Text = "Restart";
+            this.restartBtn.UseVisualStyleBackColor = true;
+            this.restartBtn.Click += new System.EventHandler(this.restartBtn_Click);
+            // 
+            // optionsBtn
+            // 
+            this.optionsBtn.Location = new System.Drawing.Point(3, 46);
+            this.optionsBtn.Name = "optionsBtn";
+            this.optionsBtn.Size = new System.Drawing.Size(101, 42);
+            this.optionsBtn.TabIndex = 1;
+            this.optionsBtn.TabStop = false;
+            this.optionsBtn.Text = "Options";
+            this.optionsBtn.UseVisualStyleBackColor = true;
+            // 
+            // leaveBtn
+            // 
+            this.leaveBtn.Location = new System.Drawing.Point(3, 94);
+            this.leaveBtn.Name = "leaveBtn";
+            this.leaveBtn.Size = new System.Drawing.Size(101, 42);
+            this.leaveBtn.TabIndex = 2;
+            this.leaveBtn.TabStop = false;
+            this.leaveBtn.Text = "Quitter";
+            this.leaveBtn.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Teal;
+            this.label1.Location = new System.Drawing.Point(1, 463);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 58);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Z\r\nS\r\n";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(376, 232);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(36, 463);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(92, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "helloooooooo";
+            this.label2.Size = new System.Drawing.Size(15, 60);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "  /\\\r\n   |\r\n\r\n   |\r\n  \\/\r\n";
             // 
-            // Form1
+            // label3
             // 
-            this.ClientSize = new System.Drawing.Size(879, 459);
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Teal;
+            this.label3.Location = new System.Drawing.Point(799, 479);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(227, 44);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Auto = M\r\nManual  = Up/Down arrows\r\n";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Tong
+            // 
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1027, 532);
+            this.Controls.Add(this.pauseMenuCtn);
+            this.Controls.Add(this.theBall);
+            this.Controls.Add(this.rightPlayer);
+            this.Controls.Add(this.leftPlayer);
+            this.Controls.Add(this.tongLogo);
+            this.Controls.Add(this.pauseInfo);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button2);
-            this.Name = "Form1";
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.scoreLeft);
+            this.Controls.Add(this.scoreRight);
+            this.MaximumSize = new System.Drawing.Size(1045, 579);
+            this.MinimumSize = new System.Drawing.Size(1045, 579);
+            this.Name = "Tong";
+            this.Text = "Tong";
+            this.Load += new System.EventHandler(this.Tong_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Tong_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Tong_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.leftPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.theBall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tongLogo)).EndInit();
+            this.pauseMenuCtn.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -64,10 +259,21 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox leftPlayer;
+        private System.Windows.Forms.PictureBox rightPlayer;
+        private System.Windows.Forms.Label scoreLeft;
+        private System.Windows.Forms.Label scoreRight;
+        private System.Windows.Forms.PictureBox tongLogo;
+        private System.Windows.Forms.Timer theTimer;
+        private System.Windows.Forms.PictureBox theBall;
+        private System.Windows.Forms.Label pauseInfo;
+        private System.Windows.Forms.FlowLayoutPanel pauseMenuCtn;
+        private System.Windows.Forms.Button restartBtn;
+        private System.Windows.Forms.Button optionsBtn;
+        private System.Windows.Forms.Button leaveBtn;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- wfa_pong/wfa_pong/Form1.cs
+++ wfa_pong/wfa_pong/Form1.cs
@@ -10,21 +10,186 @@ using System.Windows.Forms;
 
 namespace wfa_pong
 {
-    public partial class Form1 : Form
+    public partial class Tong : Form
     {
-        public Form1()
+        // Location Variables
+        int cpuDirection = 5;
+        int ballXCoordinate = 10;
+        int ballYCoordinate = 10;
+        // Score Variables
+        int playerScore = 0;
+        int cpuScore = 0;
+        // Size Variables
+        int bottomBoundary;
+        int centerPoint;
+        int xMidpoint;
+        int yMidpoint;
+        // Detection Variables
+        bool playerDetectedUp;
+        bool playerDetectedDown;
+        bool autoPlay;
+        bool cpuUp;
+        bool cpuDown;
+        // Special Keys
+        int spaceBarClicked = 0;
+
+
+        
+
+        public Tong()
         {
             InitializeComponent();
+            bottomBoundary = ClientSize.Height - leftPlayer.Height;
+            xMidpoint = ClientSize.Width / 2;
+            yMidpoint = ClientSize.Height / 2;
+            this.DoubleBuffered = true;
+            autoPlay = true;
+        }
+         private void Tong_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void theTimer_Tick(object sender, EventArgs e)
+        {
+            Random newBallSpot = new Random();
+            int newSpot = newBallSpot.Next(100, ClientSize.Height - 100);
+            // Adjust where the ball is
+            theBall.Top -= ballYCoordinate;
+            theBall.Left -= ballXCoordinate;
+            // Make the CPU move
+            if (autoPlay == true) {    
+                rightPlayer.Top += cpuDirection;
+                // Check if CPU has reached the top or the bottom
+                if (rightPlayer.Top < 0 || rightPlayer.Top > bottomBoundary) {
+                        cpuDirection = -cpuDirection; 
+                    }
+            } else
+            {
+                if (cpuUp == true && rightPlayer.Top > 0) { rightPlayer.Top -= 10; }
+                if (cpuDown == true && rightPlayer.Top < bottomBoundary) { rightPlayer.Top += 10; }
+            }
+            // Check if the ball has exited the left side of the screen
+            if (theBall.Left < 0)
+            {
+                theBall.Left = xMidpoint;
+                theBall.Top = newSpot;
+                ballXCoordinate = -ballXCoordinate;
+                cpuScore++;
+                scoreRight.Text = cpuScore.ToString();
+            }
+
+            // Check if the ball has exited the right side of the screen
+            if (theBall.Left + theBall.Width > ClientSize.Width)
+            {
+                theBall.Left = xMidpoint;
+                theBall.Top = newSpot;
+                ballXCoordinate = -ballXCoordinate;
+                playerScore++;
+                scoreLeft.Text = playerScore.ToString();
+            }
+
+            // Ensure the ball is within the boundaries of the screen
+            if (theBall.Top < 0 || theBall.Top + theBall.Height > ClientSize.Height) { ballYCoordinate = -ballYCoordinate; }
+
+            // Check if the ball hits the player or CPU paddle
+            if (theBall.Bounds.IntersectsWith(leftPlayer.Bounds) || theBall.Bounds.IntersectsWith(rightPlayer.Bounds))
+            {
+                // Send ball opposite direction
+                ballXCoordinate = -ballXCoordinate;
+            }
+
+            // Move player up
+            if (playerDetectedUp == true && leftPlayer.Top > 0) { leftPlayer.Top -= 10; }
+            // Move player down
+            if (playerDetectedDown == true && leftPlayer.Top < bottomBoundary) { leftPlayer.Top += 10; }
+            // Check for left player win
+            if (playerScore >= 10) { theTimer.Stop(); }
+            // Check for right player / CPU win
+            if (cpuScore >= 10) { theTimer.Stop(); }
+        }
+
+        private void Tong_KeyUp(object sender, KeyEventArgs e)
+                {
+                    if (e.KeyCode == Keys.Z) { playerDetectedUp = false; }
+                    if (e.KeyCode == Keys.S) { playerDetectedDown = false; }
+
+                    if (e.KeyCode == Keys.Up) { cpuUp = false; }
+                    if (e.KeyCode == Keys.Down) { cpuDown = false; }
+                }
+
+
+        private void Tong_KeyDown(object sender, KeyEventArgs e)
+        {
+            // If player presses the up arrow, move paddle upwards
+            if (e.KeyCode == Keys.Z) { playerDetectedUp = true; }
+            // If player presses the down arrow, move paddle downwards
+            if (e.KeyCode == Keys.S) { playerDetectedDown = true; }
+
+            // If Player presses the M key, toggle auto play
+            if (e.KeyCode == Keys.M)
+            { 
+                autoPlay = true;
+            }
+
+            // If Player presses the up or down arrow, toggle manual play
+            if (e.KeyCode == Keys.Up)
+            {
+                cpuUp = true;
+                autoPlay = false;
+            }   
+            if (e.KeyCode == Keys.Down)
+            {
+                cpuDown = true;
+                autoPlay = false;
+            }
+
+
+
+
+            // If player presses space bar, pause the game
+            if (e.KeyCode == Keys.Space)
+            {
+                if (spaceBarClicked % 2 == 0)
+                {
+                    theTimer.Stop();
+                    pauseInfo.Text = "Click SPACE to continue";
+                    pauseMenuCtn.Visible = true;
+                }
+                else
+                {
+                    pauseInfo.Text = "Click SPACE to pause";
+                    pauseMenuCtn.Visible = false;
+                    theTimer.Start();
+                }
+            }
+            spaceBarClicked++;
+
+
+        }
+
+        private void restartBtn_Click(object sender, EventArgs e)
+        {
+            // Reset the game
+            
+            spaceBarClicked = 0;
+            playerScore = 0;
+            cpuScore = 0;
+            scoreLeft.Text = "0";
+            scoreRight.Text = "0";
+            pauseMenuCtn.Visible = false;
+            theBall.Left = xMidpoint;
+            theBall.Top = yMidpoint;
+            ballXCoordinate = 10;
+            ballYCoordinate = 10;
+            leftPlayer.Top = yMidpoint;
+            rightPlayer.Top = yMidpoint;
+            theTimer.Start();
+            Focus();
+            
 
         }
 
         
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-            label1.Text = "0";
-
-        }
     }
 }
